7c8b4fca833e0b6c39b1daf8a64c8ed2644878e8
andful <buongiorno19972@gmail.com>
2018-11-16 05:04:31 +0100

    first commit

--- nginx/start.sh
+++ nginx/start.sh
@@ -3,7 +3,7 @@ nginx -c /app/nginx.conf -g "daemon off;"&
 TASK_PID=$!
 sleep 2
 echo "connection started"
-while ! nc -z rabbitmq 10000; do sleep 3; done
+while ! nc -z certbot 10000; do sleep 3; done
 echo "connection ended"
 sleep 3
 nginx -g "daemon off;"
