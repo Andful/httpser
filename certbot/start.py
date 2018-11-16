4b0cee197c018f2a0a37fafb4d900b563362a88f
andful <buongiorno19972@gmail.com>
2018-11-16 03:33:45 +0100

    quick fix

--- certbot/start.py
+++ certbot/start.py
@@ -8,6 +8,7 @@ from apscheduler.schedulers.blocking import BlockingScheduler
 
 running_file = "/var/www/certbot.running"
 open(running_file, 'a').close()
+print "created file"
 
 def get_domains():
     for e in glob("/servers/*/domains.txt"):
