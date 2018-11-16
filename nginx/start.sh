942736483867c3c0491235761da3690cbebed03e
andful <buongiorno19972@gmail.com>
2018-11-16 04:22:39 +0100

    socket approach

--- nginx/start.sh
+++ nginx/start.sh
@@ -1,15 +1,7 @@
 !/bin/sh
 nginx -c /app/nginx.conf -g "daemon off;"&
 TASK_PID=$!
-sleep 3
-while [ -f /var/www/certbot.running ]
-do
-  echo "file still there"
-  sleep 1
-done
-echo "file removed"
-kill $TASK_PID
-sleep 3
+nc certbot 10000
 nginx -g "daemon off;"
 
 sudo rm /var/www/certbot.running
