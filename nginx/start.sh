b4dc956345990c35e4eae4ba0cf94e3eecd3d4a1
andful <buongiorno19972@gmail.com>
2018-11-16 03:14:37 +0100

    quick fix

--- nginx/start.sh
+++ nginx/start.sh
@@ -1,9 +1,12 @@
 #!/bin/sh
 nginx -c /app/nginx.conf -g "daemon off;"&
 TASK_PID=$!
+echo TASK_PID
 sleep 1
+ls -l /var/www/certbot.running
 while [-f /var/www/certbot.running ]
 do
+  ls -l /var/www/certbot.running
   sleep 2
 done
 ls -l /var/www/certbot.running
