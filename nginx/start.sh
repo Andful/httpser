8d67f94830dced483e5baac2774e47f14201aa22
andful <buongiorno19972@gmail.com>
2018-11-16 03:21:55 +0100

    quick fix

--- nginx/start.sh
+++ nginx/start.sh
@@ -1,16 +1,12 @@
 #!/bin/sh
 nginx -c /app/nginx.conf -g "daemon off;"&
-echo $!
-export TASK_PID=$!
-echo $TASK_PID
+TASK_PID=$!
 sleep 1
-ls -l /var/www/certbot.running
 while [-f /var/www/certbot.running ]
 do
-  ls -l /var/www/certbot.running
+  echo "file still there"
   sleep 2
 done
-ls -l /var/www/certbot.running
-echo "file should be removed"
+echo "file removed"
 kill $TASK_PID
 nginx -g "daemon off;"
