4a327dd1a5d24586d792b12ff2f77f288c99e137
andful <buongiorno19972@gmail.com>
2018-11-16 05:02:18 +0100

    first commit

--- certbot/start.py
+++ certbot/start.py
@@ -1,11 +1,3 @@
-import socket
-sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-server_address = ('', 10000)
-sock.bind(server_address)
-sock.listen(1)
-c,_ = sock.accept()
-print "socket oppened"
-
 from subprocess import call
 from subprocess import check_output
 from glob import glob
@@ -55,6 +47,13 @@ for dom, exp in get_live_domain_and_exparation_date():
 for dom in domains:
     sched.add_job(get_updater(dom),'interval', days=60, next_run_time=datetime.now())
 
-print "socket closed"
+import socket
+sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+server_address = ('', 10000)
+sock.bind(server_address)
+sock.listen(1)
+c,_ = sock.accept()
+print "socket oppened"
 c.close()
+print "socket closed"
 sched.start()
--- nginx/start.sh
+++ nginx/start.sh
@@ -3,6 +3,7 @@ nginx -c /app/nginx.conf -g "daemon off;"&
 TASK_PID=$!
 sleep 2
 echo "connection started"
-yes | netcat certbot 10000
+while ! nc -z rabbitmq 10000; do sleep 3; done
 echo "connection ended"
+sleep 3
 nginx -g "daemon off;"
