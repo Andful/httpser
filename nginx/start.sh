ee3db011c00766450d6fa16265c835d1da2e9d62
andful <buongiorno19972@gmail.com>
2018-11-16 04:50:16 +0100

    first commit

--- nginx/start.sh
+++ nginx/start.sh
@@ -3,6 +3,6 @@ nginx -c /app/nginx.conf -g "daemon off;"&
 TASK_PID=$!
 sleep 2
 echo "connection started"
-netcat certbot 10000
+netcat -d certbot 10000
 echo "connection ended"
 nginx -g "daemon off;"
