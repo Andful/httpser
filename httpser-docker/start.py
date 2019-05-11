ef3822b3ea5566dbc41e393141d4a74780096631
andful <buongiorno19972@gmail.com>
2019-05-11 18:30:06 +0200

    made some changes

--- httpser-docker/start.py
+++ httpser-docker/start.py
@@ -1,47 +1,116 @@
 #!/usr/bin/python3
 
-from subprocess import call
+from subprocess import run
 from subprocess import check_output
 from glob import glob
 import os
 from datetime import datetime, timedelta
+from shutil import copyfile
 from apscheduler.schedulers.blocking import BlockingScheduler
 
 def get_domains():
-    for e in glob("/servers/*"):
+    for e in glob("/httpser/servers/*"):
         yield os.path.basename(e)
 
-def get_live_domain_and_exparation_date():
+def get_live_domains:
     certificates = glob("/etc/letsencrypt/live/*/cert.pem")
     for e in certificates:
         domain = os.path.basename(os.path.dirname(e))
-        out = check_output(["./get_exparation_date.sh", e])
-        exparation_date = datetime.strptime(out,"notAfter=%b  %d %H:%M:%S %Y GMT\n")
-        yield domain, exparation_date
+        yield domain
 
+def get_cert(domain):
+    print("getting cert for domain %s" % domain)
+    out = run([
+    "certbot",
+    "certonly",
+    "--non-interactive",
+    "--agree-tos",
+    "-m",
+    "example@mail.com",
+    "--webroot",
+    "-w",
+    "/var/www",
+    "-d",
+    domain
+    ],stdout=subprocess.DEVNULL)
+    if out.returncode != 0:
+        print("renewing of domain %s failed with code %d" % (domain, out.returncode))
 
-sched = BlockingScheduler(timezone="UTC")
-
-domains = list(get_domains())
-
-now = datetime.utcnow()
+def update(domain):
+    print("renewing cert for domain %s" % domain)
+    run([
+    "certbot",
+    "certonly",
+    "--non-interactive",
+    "--agree-tos",
+    "-m",
+    "example@mail.com",
+    "--webroot",
+    "-w", "/var/www",
+    "-d",
+    domain
+    ],stdout=subprocess.DEVNULL)
 
 def get_updater(domain):
-    def result():
-        print("renuing for domain:", domain)
-        call(["./update_cert.sh",domain])
-    return result
+    return lambda : update(domain)
 
-for dom, exp in get_live_domain_and_exparation_date():
-    if dom in domains:
+def get_exparation_date(domain):
+    certificate = "/etc/letsencrypt/live/%s/cert.pem" % domain
+    out = check_output([
+    "openssl",
+    "x509",
+    "-enddate",
+    "-noout",
+    "-in",
+    certificate])
+    return datetime.strptime(out,"notAfter=%b  %d %H:%M:%S %Y GMT\n")
+
+def reload():
+    run(["nginx", "-s", "reload"], stdout=subprocess.DEVNULL)
+
+def start():
+    run(["nginx"], stdout=subprocess.DEVNULL)
+
+reloading = os.path.isfile("/var/run/nginx.pid"):
+
+sched = BlockingScheduler(timezone="UTC")
+domains = list(get_domains())
+live_domains = list(get_live_domains())
+
+if not reloading:
+    start()
+
+for dom in domains:
+    if dom in live_domains:
         domains.remove(dom)
-        date_to_renew= exp - timedelta(days=30)
-        if date_to_renew - now > timedelta(days=1):
-            print "scheduling for domain:", dom, "\trenew due:", date_to_renew
+        exp = get_exparation_date(dom)
+        date_to_renew = exp - timedelta(days=30)
+        copyfile(os.path.join("/httpser/servers",dom,"http."+dom+".conf"),"/httpser/actives")
+        copyfile(os.path.join("/httpser/servers",dom,"https."+dom+".conf"),"/httpser/actives")
+
+        reload()
+        if date_to_renew - datetime.utcnow() > timedelta(days=1): # no need to update now
+
+            print("scheduling renewing for domain %s with renew date %s" % (dom, date_to_renew))
             sched.add_job(get_updater(dom),'interval', days=60, next_run_time=date_to_renew)
-        else:
-            to_run = get_updater(dom)
-            sched.add_job(to_run,'interval', days=60)
-            to_run()
+        else: #need to update now
+            print("scheduling renewing for domain %s with renew date %s" % (dom, date_to_renew + timedelta(days=60)))
+            sched.add_job(get_updater(dom),'interval', days=60)
+            update(dom)
+    else:
+        copyfile(os.path.join("/httpser/servers",dom,"http."+dom+".conf"),"/actives")
+        reload()
+        print("scheduling renewing for domain %s with renew date %s" % (dom, date_to_renew + timedelta(days=60)))
+        sched.add_job(get_updater(dom),'interval', days=60)
+        get_cert(dom)
+        copyfile(os.path.join("/servers",dom,"https."+dom+".conf"),"/actives")
+        reload()
+        print("%s is online" % dom)
+
+for e in glob("/httpser/actives/*"):
+    first_dot = e.find('.')
+    saved_file_domain = e[first_dot + 1:-len(".conf")]
+    if saved_file_domain not in domains:
+        os.remove(e)
 
 sched.start()
